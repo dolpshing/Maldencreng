420bb6557b064256efaf6a74eb62e405852f67f4
dolpshing <ryuki.parker@gmail.com>
2019-12-07 19:24:43 -0800

    added the upgrade section
    upgrade section with a list of upgrades inside
    list is scrollable

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const cpsText = document.querySelector('#cps');
 const begCpcText = document.querySelector('#beg-cpc');
 const begButton = document.querySelector('#beg-button');
 
+// Upgrades
+const upgrade1 = document.querySelector('#upgrade1');
+const upgrade1Text = document.querySelector('#upgrade1-text');
+const upgrade2 = document.querySelector('#upgrade2');
+const upgrade2Text = document.querySelector('#upgrade2-text');
+
 // Game Variables
 let croins = 0; // Number of Croins
 let maldbucks = 0; // Number of Maldbucks
@@ -15,12 +21,17 @@ let cps = 0; // Croins per second
 
 // Main function
 function main() {
+  startupFunctions();
+  checkUpgrades();
+}
+
+main();
+
+function startupFunctions() {
   updateInfo();
   checkForBeg();
   addCPS();
 }
-
-main();
 
 // Updates in-game info
 function updateInfo() {
@@ -42,6 +53,28 @@ function addCPS() {
 function checkForBeg() {
   begButton.addEventListener('click', () => {
     croins += cpc;
+    updateInfo();
+  })
+}
+
+// Check if upgrades have been clicked
+function checkUpgrades() {
+  checkUpgrade1();
+  checkUpgrade2();
+}
+
+// Upgrade 1
+function checkUpgrade1() {
+  upgrade1.addEventListener('click', () => {
+    cpc += 1;
+    updateInfo();
+  })
+}
+
+// Upgrade 2
+function checkUpgrade2() {
+  upgrade2.addEventListener('click', () => {
+    cps += 1;
     updateInfo();
   })
 }
